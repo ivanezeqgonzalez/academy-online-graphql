5207e83b8b3c734127c50ca8f6fc9108dbe85d42
Ivan Gonzalez <ivanezeq.gonzalez@gmail.com>
2020-04-12 18:11:24 -0300

    Query student

--- src/resolvers/query.ts
+++ src/resolvers/query.ts
@@ -5,6 +5,18 @@ const query: IResolvers = {
     Query: {
         students(): any {
             return database.students
+        },
+        student(__: void, { id }): any {
+            const resultado = database.students.filter(student => student.id === id)[0]
+            if (resultado === undefined) {
+                return {
+                    id: '-1',
+                    name: `Student with id: ${id} not found`,
+                    email:'',
+                    courses: []
+                }
+            }
+            return resultado;
         }
     }
 }
